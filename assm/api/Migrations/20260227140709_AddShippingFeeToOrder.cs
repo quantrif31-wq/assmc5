808a02b2381c31f87895fbde4a3fc05b6d0e8289
thầy vàng <141602381+houkillDariky@users.noreply.github.com>
2026-02-27 21:16:37 +0700

    push testcase

---
+++ assm/api/Migrations/20260227140709_AddShippingFeeToOrder.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab4.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShippingFeeToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "District",
+                table: "Orders",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ShippingFee",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "District",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingFee",
+                table: "Orders");
+        }
+    }
+}
